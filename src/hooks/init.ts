699814092c0a84d5132357b167bd9e6fca76b57f
Max Isom <hi@maxisom.me>
2021-01-04 15:07:13 -0600

    Fix .env with pkg

--- src/hooks/init.ts
+++ src/hooks/init.ts
@@ -1,9 +1,10 @@
 import {Hook} from '@oclif/config';
+import * as path from 'path';
 
 const hook: Hook<'init'> = async function (options) {
 	// Load environment variables
 	// eslint-disable-next-line @typescript-eslint/no-var-requires
-	require('dotenv').config();
+	require('dotenv').config({path: path.join(__dirname, '../../.env')});
 };
 
 export default hook;
